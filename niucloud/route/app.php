32d591d1084bfddde0c7964e27b37619e9b06776
全栈小学生 <1518079521@qq.com>
2023-05-20 18:32:23 +0800

    Update app.php

--- niucloud/route/app.php
+++ niucloud/route/app.php
@@ -1,10 +1,24 @@
 <?php
-
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
 use think\facade\Route;
+use think\facade\Request;
+
 Route::domain('install.php', ':\app\install\controller');
 // 访问首页自动跳转到admin
 Route::rule('/', function () {
-    return redirect('/web');
+    if (Request::isMobile()) {
+        return redirect('/wap');
+    } else {
+        return redirect('/web');
+    }
 });
 // 管理后台
 Route::rule('admin/:any', function () {
@@ -14,7 +28,7 @@ Route::rule('admin/:any', function () {
 Route::rule('wap/:any', function () {
     return view(app()->getRootPath() . 'public/wap/index.html');
 })->pattern(['any' => '\w+']);
-// 手机端
+// 电脑端
 Route::rule('web/:any', function () {
     return view(app()->getRootPath() . 'public/web/index.html');
 })->pattern(['any' => '\w+']);
