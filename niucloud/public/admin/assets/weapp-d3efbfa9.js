2c0ae78f6c07fc22b660e070ace338472e5d1d58
全栈小学生 <1518079521@qq.com>
2023-06-03 18:35:15 +0800

    update admin

--- niucloud/public/admin/assets/weapp-d3efbfa9.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./index-6ef4c1c3.js";function p(){return e.get("weapp/config")}function a(t){return e.put("weapp/config",t,{showSuccessMessage:!0})}function n(){return e.get("weapp/template")}function u(t){return e.put("weapp/template/sync",t,{showSuccessMessage:!0})}export{n as a,u as b,p as g,a as s};
